b68d589ebf756a2e6f4798a8a974b92ffaf090d0
jade <jadeli2166@yahoo.com>
2024-04-04 22:16:21 -0700

    competed calc

--- calc/app.py
+++ calc/app.py
@@ -1,1 +1,49 @@
-# Put your app in here.
+from flask import Flask
+from operations import add, sub, mult, div
+
+app = Flask(__name__)
+
+@app.route('/add')
+def to_add():
+    a = int(request.args.get('a'))
+    b = int(request.args.get('b'))
+    result = add(a,b)
+    return str(result)
+
+@app.route('/sub')
+def to_sub():
+    a = int(request.args.get('a'))
+    b = int(request.args.get('b'))
+    result = sub(a,b)
+    return str(result)
+
+@app.route('/mult')
+def to_mult():
+    a = int(request.args.get('a'))
+    b = int(request.args.get('b'))
+    result = mult(a,b)
+    return str(result)
+
+@app.route('/div')
+def to_div():
+    a = int(request.args.get('a'))
+    b = int(request.args.get('b'))
+    result = div(a,b)
+    return str(result)
+
+#further study
+
+operators = {
+    'add': add,
+    'sub': sub,
+    'mult': mult,
+    'div': div,
+}
+
+@app.route('/math/<oper>')
+def do_math(oper):
+    a = int(request.args.get('a'))
+    b = int(request.args.get('b'))
+    result = operators[oper](a,b)
+
+    return str(result)
